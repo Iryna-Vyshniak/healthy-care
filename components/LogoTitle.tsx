1f5b5a18ce553ef99db48d48aae7951c7762ec9a
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-07-19 23:51:01 +0300

    feat: Implemented LogoTitle component

---
+++ components/LogoTitle.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LogoTitle = () => {
+  return (
+    <div className='bg-gradient-custom clip-text drop-shadow-[0_0.5px_0.05px_rgba(0,0,20,1)] dark:drop-shadow-[0_0.5px_0.05px_rgba(250,250,250,1)]'>
+      <p className='text-24-bold'>
+        Health<span>Care</span>
+      </p>
+    </div>
+  );
+};
+
+export default LogoTitle;
