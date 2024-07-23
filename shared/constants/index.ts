a85ca4a391377ddd4ef60b9dec1a90b77d656f85
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-07-23 21:06:14 +0300

    feat: Added new gender options and patient form default values. Updated constants.ts to include comprehensive default values for patient forms and expanded gender options.

---
+++ shared/constants/index.ts
@@ -0,0 +1,36 @@
+export enum FormFieldType {
+    INPUT = "input",
+    TEXTAREA = "textarea",
+    PHONE_INPUT = "phoneInput",
+    CHECKBOX = "checkbox",
+    DATE_PICKER = "datePicker",
+    SELECT = "select",
+    GROUP = "group",
+}
+
+export const GenderOptions = ['Male', 'Female', 'Non-binary', 'Prefer not to say', 'Only disclose to doctor'];
+
+export const PatientFormDefaultValues = {
+    name: "",
+    email: "",
+    phone: "",
+    birthDate: new Date(Date.now()),
+    gender: "Male" as Gender,
+    address: "",
+    profession: "",
+    emergencyContactName: "",
+    emergencyContactNumber: "",
+    primaryPhysician: "",
+    insuranceProvider: "",
+    insurancePolicyNumber: "",
+    allergies: "",
+    currentMedication: "",
+    familyMedicalHistory: "",
+    pastMedicalHistory: "",
+    identificationType: "Birth Certificate",
+    identificationNumber: "",
+    identificationDocument: [],
+    treatmentConsent: false,
+    disclosureConsent: false,
+    privacyConsent: false,
+};
