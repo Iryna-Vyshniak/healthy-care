040aef5fcc91a8d07060c4f81cb1c50a29228cff
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-07-23 21:10:54 +0300

    feat: Implemented RenderField component

---
+++ components/fields/RenderField.tsx
@@ -0,0 +1,95 @@
+import DatePicker from 'react-datepicker';
+// International phone number <input/> for React
+import PhoneInput from 'react-phone-number-input';
+import { E164Number } from 'libphonenumber-js';
+
+import 'react-phone-number-input/style.css';
+import 'react-datepicker/dist/react-datepicker.css';
+
+import { FormControl } from '@/components/ui/form';
+import Icon, { iconSizes } from '@/components/ui/icon';
+import { Input } from '@/components/ui/input';
+
+import { FormFieldType } from '@/shared/constants';
+
+const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
+  const {
+    iconSrc,
+    iconAlt,
+    fieldType,
+    placeholder,
+    name,
+    label,
+    showTimeSelect,
+    dateFormat,
+    renderGroup,
+  } = props;
+  switch (fieldType) {
+    case FormFieldType.INPUT:
+      return (
+        <div className='flex items-center justify-start gap-1 rounded-[13px] border border-dashed border-blue-500 bg-blue-500 dark:border-dark-500 dark:bg-dark-400'>
+          {iconSrc && (
+            <Icon
+              name={iconSrc.name}
+              size={iconSizes.MEDIUM}
+              type={iconSrc.type}
+              className='mx-2'
+            />
+          )}
+          <FormControl>
+            <Input placeholder={placeholder} {...field} className='shad-input border-0' />
+          </FormControl>
+        </div>
+      );
+    case FormFieldType.PHONE_INPUT:
+      return (
+        <div className='flex items-center justify-start rounded-[13px] border border-dashed border-blue-500 bg-blue-500 dark:border-dark-500 dark:bg-dark-400'>
+          {' '}
+          <PhoneInput
+            international
+            defaultCountry='UA'
+            withCountryCallingCode
+            placeholder={placeholder}
+            value={field.value as E164Number | undefined}
+            onChange={field.onChange}
+            {...field}
+            className='input-phone'
+          />
+        </div>
+      );
+    case FormFieldType.GROUP:
+      return renderGroup ? renderGroup(field) : null;
+    case FormFieldType.DATE_PICKER:
+      return (
+        <div className='flex items-center justify-start rounded-[13px] border border-dashed border-blue-500 bg-blue-500 dark:border-dark-500 dark:bg-dark-400'>
+          {' '}
+          {iconSrc && (
+            <Icon
+              name={iconSrc.name}
+              size={iconSizes.MEDIUM}
+              type={iconSrc.type}
+              className='w-[24px] h-[24px] mx-2'
+            />
+          )}
+          <FormControl>
+            <DatePicker
+              selected={field.value}
+              onChange={(date) => field.onChange(date)}
+              showTimeSelect={showTimeSelect ?? false}
+              timeInputLabel='Time:'
+              dateFormat={dateFormat ?? 'MM/dd/yyyy'}
+              wrapperClassName='date-picker'
+            />
+          </FormControl>
+        </div>
+      );
+    // case FormFieldType.SKELETON:
+    //   return (
+    //     <div className='flex items-center justify-start rounded-[13px] border border-dashed border-blue-500 bg-blue-500 dark:border-dark-500 dark:bg-dark-400'></div>
+    //   );
+    default:
+      break;
+  }
+};
+
+export default RenderField;
