ec2957240a099c976ee514832aea29ef7c7862e3
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-08-27 09:21:23 +0300

    feat: Added Identification options array for user verification
    - Added a constant array `Identification` containing various types of identification options.
    - This array includes items such as Passport, National Identity Card, Driver's License, and more.
    - The list will be used for user identification and verification purposes within the application.

---
+++ shared/data/identification.ts
@@ -0,0 +1,13 @@
+export const Identification = [
+  'Passport',
+  'National Identity Card',
+  "Driver's License",
+  'State ID Card',
+  'Resident Alien Card (Green Card)',
+  'Social Security Card',
+  'Voter ID Card',
+  'Military ID Card',
+  'Student ID Card',
+  'Birth Certificate',
+  'Medical Insurance Card/Policy',
+];
