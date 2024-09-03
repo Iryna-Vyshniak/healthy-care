9f3f901e7d8a74c35077b7577054b53e77dba8f5
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-09-03 22:45:54 +0300

    refactor: Updated LogoTitle component to support optional aftersubtitle
    - Added `aftersubtitle` prop to the `LogoTitle` component for additional subtitle content.
    - Updated styles: applied `dark:clip-text`, adjusted text alignment with `ml-2`, and added conditional rendering for `aftersubtitle`.
    - Modified typography to use `font-yanone`, adjusted text sizes, and applied color changes for better visual consistency.
    - Removed the span around `subtitle` and updated the structure to improve readability.

--- components/common/LogoTitle.tsx
+++ components/common/LogoTitle.tsx
@@ -3,15 +3,24 @@ import React from 'react';
 const LogoTitle = ({
   title,
   subtitle,
+  aftersubtitle,
 }: {
   title: string;
   subtitle: string;
+  aftersubtitle?: string;
 }) => {
   return (
-    <div className='clip-text bg-gradient-custom drop-shadow-[0_0.5px_0.05px_rgba(0,0,20,1)] dark:drop-shadow-[0_0.5px_0.05px_rgba(250,250,250,1)]'>
-      <p className='text-24-bold'>
+    <div className='dark:clip-text ml-2 text-dark-200 drop-shadow-[0_0.5px_0.05px_rgba(250,250,250,1)] dark:text-white'>
+      <p className='mr-2 font-yanone text-[10px] font-bold uppercase dark:text-white'>
         {title}
-        <span>{subtitle}</span>
+      </p>
+      <p className='mr-2 font-yanone text-[10px] font-bold uppercase text-green-600'>
+        {subtitle}
+        {aftersubtitle && (
+          <span className='mr-2 font-yanone text-[10px] font-bold uppercase text-dark-200 dark:text-white'>
+            {aftersubtitle}
+          </span>
+        )}
       </p>
     </div>
   );
