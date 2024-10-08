445d6e8521b2b5517b0cfc90f9289efaf5568759
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-10-08 20:33:16 +0300

    feat(appointment.actions.ts): Added getAppointment function to retrieve appointment details
    - Implemented `getAppointment` function to fetch a specific appointment using the `appointmentId` from the Appwrite database.
    - Utilized `databases.getDocument` to retrieve the appointment document associated with the provided ID.
    - Added error handling to log any issues encountered during the retrieval process.
    - Enhanced appointment management by returning the parsed and stringified appointment data.

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -1,3 +1,5 @@
+'use server';
+
 import { revalidatePath } from 'next/cache';
 
 import { ID } from 'node-appwrite';
@@ -25,5 +27,23 @@ export const createAppointment = async (
     return parseStringify(newAppointment);
   } catch (error) {
     console.error('An error occurred while creating a new appointment:', error);
+  }
+};
+
+// GET APPOINTMENT
+export const getAppointment = async (appointmentId: string) => {
+  try {
+    const appointment = await databases.getDocument(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      appointmentId
+    );
+
+    return parseStringify(appointment);
+  } catch (error) {
+    console.error(
+      'An error occurred while retrieving the existing patient:',
+      error
+    );
   }
 };
